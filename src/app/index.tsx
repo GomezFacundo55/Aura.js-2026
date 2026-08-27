df6d940622ee5676cfe97a2e384b72d24017d127
GiuDelgo <delgobbogiuliana@gmail.com>
2026-08-27 16:51:38 -0300

    add: estructura del proyecto

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View, StyleSheet } from "react-native";
+import '../../global.css';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-neutral-400">
+      <Text className="text-xl font-sans-bold text-brand-900">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
