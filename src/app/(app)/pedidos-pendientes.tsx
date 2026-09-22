7377976ed523b9561c7c97f92b022331ec42b4ea
EnzoJauregui <enzo97zj@gmail.com>
2026-09-21 22:21:42 -0300

    Mejora de la pantalla de pedidos pendientes de cocinero y bartender con paginacion . Aun falta agregar logica de cambio de estado de los pedidos

--- src/app/(app)/pedidos-pendientes.tsx
+++
@@ -1,5 +0,0 @@
-import PedidosPendientesScreen from "@/screensJ/pedidos-pendientes";
-
-export default function PedidosProductosLayout(){
-    return <PedidosPendientesScreen></PedidosPendientesScreen>
-}
--- src/screensJ/dashboard-productos.tsx
+++ src/screensJ/dashboard-productos.tsx
@@ -211,13 +211,15 @@ export default function ProductDashboard() {
       <View>
         <TouchableOpacity
           activeOpacity={0.8}
-          onPress={() => router.push({
-            pathname: "/(app)/pedidos-pendientes",
-            params : { perfil: perfilUsuario?.perfil }
-          })}
+          onPress={() => {
+            let ruta: any = perfilUsuario?.perfil === "cocinero" ? "/(app)/pedidos-cocina" : "/(app)/pedidos-bar"
+            router.push({
+              pathname: ruta,
+              params : { perfil: perfilUsuario?.perfil }
+          })}}
           className="flex-row items-center bg-brand-500 px-3.5 py-2.5 mb-1 rounded-xl shadow-sm"
         >
-          <Ionicons name="add-circle-outline" size={18} color="#FFFFFF" />
+          <Ionicons name="list-circle-outline" size={18} color="#FFFFFF" />
           <Text className="text-white font-semibold text-sm ml-1.5">Pendientes</Text>
         </TouchableOpacity>
         <TouchableOpacity
